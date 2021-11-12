d086bd2685fa0b26c206215f8b803fab154b74be
Maria Sudermann <maria.sudermann@gmail.com>
2021-11-12 12:29:55 +0100

    feat (db) : update knex migrations

--- server/db/migrations/20211111195403_init.js
+++ server/db/migrations/20211111195403_init.js
@@ -1,12 +1,38 @@
 exports.up = function (knex) {
-  return knex.schema.createTable('strays', function (table) {
-    table.increments('id');
-    table.string('name').notNullable();
-    table.string('sex');
-    table.string('colour');
-  });
+  return knex.schema
+    .createTable('spots', function (table) {
+      table.increments('id');
+      table.float('lat');
+      table.float('long');
+    })
+    .createTable('strays', function (table) {
+      table.increments('id');
+      table.string('name').notNullable();
+      table.string('sex');
+      table.string('colour');
+      table.integer('spot_id').unsigned().notNullable();
+      table.foreign('spot_id').references('id').inTable('spots');
+    })
+    .createTable('users', function (table) {
+      table.increments('id');
+      table.string('name').notNullable();
+      table.string('email').notNullable();
+      table.string('password').notNullable();
+    })
+    .createTable('feedings', function (table) {
+      table.increments('id');
+      table.timestamp('created_at').defaultTo(knex.fn.now());
+      table.integer('spot_id').unsigned().notNullable();
+      table.foreign('spot_id').references('id').inTable('spots');
+      table.integer('user_id').unsigned().notNullable();
+      table.foreign('user_id').references('id').inTable('users');
+    });
 };
 
 exports.down = function (knex) {
-  return knex.schema.dropTableIfExists('strays');
+  return knex.schema
+    .dropTableIfExists('strays')
+    .dropTableIfExists('users')
+    .dropTableIfExists('spots')
+    .dropTableIfExists('feedings');
 };
