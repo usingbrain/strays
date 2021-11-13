44ca9c2ed69d517a2a3af0f5c3f4b24bd6d028b8
Maria Sudermann <maria.sudermann@gmail.com>
2021-11-13 11:33:36 +0100

    feat (map): load with current position

--- client/src/components/map/Map.js
+++ client/src/components/map/Map.js
@@ -3,11 +3,37 @@ import ReactMapGL, { Marker, Popup } from 'react-map-gl';
 import './Map.css';
 
 function Map() {
+  let startPosition = {};
+
+  function geoSuccess(position) {
+    startPosition.lat = parseFloat(position.coords.latitude);
+    startPosition.long = parseFloat(position.coords.longitude);
+    setViewport({
+      latitude: startPosition.lat,
+      longitude: startPosition.long,
+      zoom: 12,
+    });
+  }
+
   const [viewport, setViewport] = useState({
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8,
+    latitude: startPosition.lat,
+    longitude: startPosition.long,
+    zoom: 12,
   });
+  function geoError(error) {
+    console.error('Error code ' + error.code + ': ' + error.message);
+  }
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+  } else {
+    console.log('Geolocation API Not Supported');
+    // 52.5075, 13.3781 - codeworks Berlin
+    startPosition.lat = 52.5075;
+    startPosition.long = 13.3781;
+  }
+
+  console.log(startPosition);
 
   // frontend is not accessing .env right now. This works when token is put in directly
 
@@ -15,7 +41,7 @@ function Map() {
     <div className="map">
       <ReactMapGL
         {...viewport}
-        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+        mapboxApiAccessToken="pk.eyJ1IjoidXNpbmdicmFpbiIsImEiOiJja3Z3czZ5angwaGN4Mm9vZXdtMnB0MGthIn0.NOgkl7tfJTSkwEDcgTovSw"
         width="100%"
         height="100%"
         onViewportChange={(viewport) => setViewport(viewport)}
